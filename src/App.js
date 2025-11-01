baa44bb59686a8e9766e0502ac54610a12991f66
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2025-11-02 03:50:35 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
-import RestaurantCard  from "./components/RestaurantCard.js";
+// import RestaurantCard  from "./components/RestaurantCard.js";
 
 
 
