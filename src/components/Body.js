36f8c6483ab8f60f47e627eba88d068371878f00
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2025-11-02 05:14:45 +0530

    simple filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,23 +3,29 @@ import ReactDOM from "react-dom/client";
 import RestaurantCard from "./RestaurantCard";
 import  restaurantList  from "../utils/mockData.js";
 
-const Body = () => 
-(
-    <div style={{backgroundColor: "#8080F0"}}>
-        <div className="search-bar">Search</div>
-        <div className="rest-container">
-		{
-			restaurantList.map((restaurant) => 
-				(
-					<RestaurantCard key={restaurant.info.id} resData={restaurant} ></RestaurantCard>
-				)
-			)
-			
-		}
-            
-            
-        </div>
-    </div>
-);
+const Body = () => {
+
+		let newRestaurantList = [];
+		newRestaurantList = restaurantList.filter(restaurant => restaurant.info.avgRating > 4);
+
+		return  (
+			<div style={{backgroundColor: "#8080F0"}}>
+				<div className="search-bar">Search</div>
+				<div className="filter-btn-toprated">
+					<button onClick={()=> console.log("Top Rated Clicked")}>Top Rated Restaurants</button>
+				</div>
+				<div className="rest-container">
+				{
+					newRestaurantList.map((restaurant) => 
+						(
+							<RestaurantCard key={restaurant.info.id} resData={restaurant} ></RestaurantCard>
+						)
+					)
+					
+				}
+				</div>
+			</div>
+		);
+}
 
 export default Body;
