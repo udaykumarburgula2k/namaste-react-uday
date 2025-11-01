5603d0ed1a2bc438d5725e67cb926c0fcc8c0ffb
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2025-11-01 20:41:36 +0530

    react element and react component, react composition

--- App.js
+++ App.js
@@ -43,6 +43,26 @@ const Title = function() {
                 );
 }
 
+const SubTitle2 = <span>MySubTitle2</span>;
+
+// React element Title2
+const Title2 =  (
+                <h1>
+                    Nameste from element TitleComponent!! with function..
+                    SubTitle2 {SubTitle2}
+                </h1>
+                );
+
+/*
+
+const response = fetch("https://api.example.com/data");
+
+{response} <- if it returns some response it will prevent cross site scripting attacks.
+              React santizes the data before rendering it to the DOM.
+*/
+
+
+const number = 100;
 
 // component composition - using one component inside another component
 
@@ -51,6 +71,10 @@ const HeadingComponent = () => {
     return (
         <div>
             <Title />
+            <Title >Overriding Title</Title> {/* you can override component content using children props */}
+            <h2>{number+1}</h2> {/* embedding JavaScript expression inside JSX using {} */}
+            {console.log("Inside JSX")} {/* you can also use statements like console.log inside JSX */}
+            {Title2} {/* embedding React element inside JSX */}
             Nameste from HeadingComponent!!
         </div>
     );
@@ -61,6 +85,10 @@ const HeadingComponent2 = () => (
     <h1 id="jsx-heading" className="header" tabIndex="2" >
         Nameste JSX--
     </h1>);
+
+/*
+It will infinite loop if you add HeadingComponent in Title & add Title in HeadingComponent
+*/
 
 // React Functional Component - Equivalent to above code - single line JSX does not need parentheses
 const HeadingComponent3 = () => <h1 id="jsx-heading" className="header" tabIndex="2" >Nameste JSX--</h1>;
