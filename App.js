aaa0a345db99ca62a5cb8394559fe41f6453f023
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2025-11-01 12:39:51 +0530

    React ReactDOM basic heading createElement createRoot

--- App.js
+++ App.js
@@ -1,51 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/*
-* 
-*  <div id="parent">
-*      <div id="child">
-*          <h1>I am h1 tag</h1> 
-*          <h2>I am h2 tag</h2>
-*      </div>
-*    <div id="child2">
-*          <h1>I am h1 tag</h1> 
-*          <h2>I am h2 tag</h2>
-*      </div>
-*  </div>
-*   
-*/
 
-const parent = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "I am h1 tag.123"),
-        React.createElement("h2", {}, "I am h2 tag")]
-    ),
-    React.createElement("div", { id: "child2" },
-        [React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")]
-    )
-]);
+// React element - smallest building block of React app, it is not HTML element.
+// React element is an object when we render it using ReactDOM it becomes HTML element
 
-console.log(parent); // prints object - react element object
-
-// React element object => HTML (using ReactDOM which browser understands)
-
-var root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); // react object converted to html and displayed inside root div
-    
+const heading = React.createElement("h1", { id: "heading" }, "Nameste React--");
 
 
-// const heading = React.createElement("h1",
-//     {
-//         id: "heading",
-//         myOwnElementType: "h1"
-//     },
-//     "Hello World! from React");
-//     console.log(heading); // prints object
-// var root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading); // react object converted to html and displayed inside root div
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// React.createElement() => Object => HTML (using ReactDOM)
+root.render(heading); // React element object converted to HTML and displayed inside root div
 
-// JSX - JavaScript XML
+
