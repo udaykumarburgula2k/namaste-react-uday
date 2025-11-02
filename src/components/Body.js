193569bd9a21afb4147744a05d4aacd27c2126bf
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2025-11-02 07:43:28 +0530

    Filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,16 +3,28 @@ import ReactDOM from "react-dom/client";
 import RestaurantCard from "./RestaurantCard";
 import  restaurantList  from "../utils/mockData.js";
 
+// const filterData = (searchText, restaurantList) => {
+// 	console.log("Top Rated Clicked")
+// 	return restaurantList.filter((restaurant) =>
+// 		newRestaurantList = restaurantList?.filter(restaurant => restaurant.info.avgRating > 4)
+// 	);
+// }
+
 const Body = () => {
 
-		let newRestaurantList = [];
-		newRestaurantList = restaurantList.filter(restaurant => restaurant.info.avgRating > 4);
+		let newRestaurantList = restaurantList;
+		
 
 		return  (
 			<div style={{backgroundColor: "#8080F0"}}>
 				<div className="search-bar">Search</div>
 				<div className="filter-btn-toprated">
-					<button onClick={()=> console.log("Top Rated Clicked")}>Top Rated Restaurants</button>
+					<button onClick={()=> {
+						console.log("Top Rated Clicked");
+						newRestaurantList = restaurantList?.filter(restaurant => restaurant.info.avgRating > 4);
+						console.log(newRestaurantList);
+
+					} }>Top Rated Restaurants</button>
 				</div>
 				<div className="rest-container">
 				{
